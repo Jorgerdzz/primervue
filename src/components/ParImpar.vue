d7861d977c2c5d86976bb3a5646fa5f3917828e7
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-24 14:24:24 +0200

    ejercicio par impar

---
+++ src/components/ParImpar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>PAR IMPAR</h1>
+        <label>Introduzca un número:</label><br>
+        <input type="number" v-model=""/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ParImpar",
+        data(){
+            return{
+                numero: 0,
+            }
+        },
+        methods: {
+            parImpar(){
+                
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
