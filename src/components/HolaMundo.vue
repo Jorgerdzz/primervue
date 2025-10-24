5e105201a518e5452a961f49c293dc1aa023dc79
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-24 09:55:19 +0200

    mi primer vue

---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Mi primer vue, que ilusión!!!</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HolaMundo"
+    }
+</script>
+
+<style>
+    h1 {color: blue}
+</style>
