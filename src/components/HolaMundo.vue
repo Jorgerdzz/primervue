6c5244a3b146fae41ff8e470d98238c26d74a36d
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-24 10:12:24 +0200

    pintar variables en vue

--- src/components/HolaMundo.vue
+++ src/components/HolaMundo.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
         <h1>Mi primer vue, que ilusión!!!</h1>
+        <h2> Mi nombre es {{nombre}}</h2>
+        <h2> Mi edad es {{edad}}</h2>
+        <h2></h2>
     </div>
 </template>
 
 <script>
     export default {
-        name: "HolaMundo"
+        name: "HolaMundo",
+        data () {
+            return {
+                nombre: "Jorge",
+                edad: 25,
+                array: [],
+                objeto: {}
+            }
+        }
     }
 </script>
 
